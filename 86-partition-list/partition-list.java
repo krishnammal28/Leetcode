89306fb8fb275228eb72561d72383dbb2507b2f8
krishnammal28 <krishnammalsamuthirapandian@gmail.com>
2026-05-13 14:56:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (67.03%) - LeetSync

---
+++ 86-partition-list/partition-list.java
@@ -0,0 +1,30 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode beforeHead = new ListNode(0);
+        ListNode afterHead = new ListNode(0);
+
+        ListNode before = beforeHead;
+        ListNode after = afterHead;
+
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            if (curr.val < x) {
+                before.next = curr;
+                before = before.next;
+            } else {
+                after.next = curr;
+                after = after.next;
+            }
+
+            curr = curr.next;
+        }
+
+        after.next = null;
+        before.next = afterHead.next;
+
+        return beforeHead.next;
+    }
+}
